faae66f2f7fe4d3099bc60eeb49ea619775d96cc
Joet2022 <johnetornam2@gmail.com>
2022-10-17 15:26:02 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = '0';
+	while
+		(c >= 'a') {
+			putchar(c);
+			c--;
+		}
+	putchar('\n');
+	return (0);
+}
