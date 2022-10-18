0ab9af0c6caab987c40f3125c1d03f0272012243
Joet2022 <johnetornam2@gmail.com>
2022-10-17 17:07:31 -0700

    my first c line

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (Success)
+/*
+ * main - print a line of code usings puts
+ *
+ * Return: 0
  */
-
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
