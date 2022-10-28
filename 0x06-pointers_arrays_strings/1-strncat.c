b217d4298307ea109a9967ec17357fb20c1d5365
Joet2022 <johnetornam2@gmail.com>
2022-10-27 17:08:09 -0700

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,10 +6,12 @@
  * @dest: copy to
  * @src: copy from
  * @n: number of char to be copied
- * Return: dest
+ * Return: string
  */
 char *_strncat(char *dest, char *src, int n);
 {
+	char dest[100] = "Hello";
+	char *src = "World";
 	strncat(dest, src, n);
 	return (dest);
 }
