f010d0b38e944584a97eafe54a5ced7bcc796963
Joet2022 <johnetornam2@gmail.com>
2022-10-27 18:48:11 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,28 @@
 #include "main.h"
 #include <string.h>
+
 /**
  * _strncpy - for copying purpose
  * @dest: param1
  * @src: param2
  * @n: param3
- * Return: string
+ * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, n);
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+			j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
 	return (dest);
+}
