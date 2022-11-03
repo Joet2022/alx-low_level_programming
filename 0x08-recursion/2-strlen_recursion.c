bd13ffa1ccf1a019257ef59d153ed90cac92cdba
Joet2022 <johnetornam2@gmail.com>
2022-11-03 05:36:27 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen_recursion -> function to get length of string
- * @s: - string parameter
+ * @s: - string param
  * Return: length of string
  */
 int _strlen_recursion(char *s)
@@ -10,5 +10,5 @@ int _strlen_recursion(char *s)
 	if (*s == '\0')
 		return (0);
 	s++;
-	return (1 + strlen recursion(s));
+	return (1 + _strlen_recursion(s));
 }
